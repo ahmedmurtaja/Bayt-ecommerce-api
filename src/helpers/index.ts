0029a90cd2fd4a436578fead5a3b7211dcc0c198
Ahmed Izz Murtaja <56529633+ahmedmurtaja@users.noreply.github.com>
2023-08-07 15:01:20 +0300

    chore: remove unsed imports

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,12 +1,5 @@
 import { CustomError, templateErrors } from './CustomError';
 
-import {
-  getAppointmentSchema,
-  updateAvailableSchema, therapistInfoSchema, addAppointmentSchema,
-  adminLoginSchema, updateTherapistActiveSchema,
-} from './validation';
-
 export {
-  CustomError, templateErrors, getAppointmentSchema, updateAvailableSchema, therapistInfoSchema,
-  addAppointmentSchema, adminLoginSchema, updateTherapistActiveSchema,
+  CustomError, templateErrors,
 };
